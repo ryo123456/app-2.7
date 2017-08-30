c2e8e6eab83074b521df65a53b29a523b04025bb
root <root@localhost.localdomain>
2017-08-30 16:00:19 +0900

    住所変換

--- testproject/mapapp/views.py
+++ testproject/mapapp/views.py
@@ -4,12 +4,36 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import MyForm
-# Create your views here.
-#def index(request):
-   # return render(request, 'mapapp/index.html')
-
+import geocoder
+import urllib
+import xml.dom.minidom
 def form_test(request):
-    form = MyForm()
-    return render(request, 'mapapp/index.html', {
+    	if request.method == "POST":
+        	form = MyForm(data=request.POST)  
+        	if form.is_valid():  
+            		w=request.POST['text']
+			a=geocode(w)
+			dom = xml.dom.minidom.parseString(a)
+			location = dom.getElementsByTagName('location')
+			if location.length > 0:
+        			lat = location[0].getElementsByTagName('lat')[0].firstChild.data
+        			lng = location[0].getElementsByTagName('lng')[0].firstChild.data
+				print(lat,lng)	
+		 
+    	else:  
+        	form = MyForm()
+    	return render(request, 'mapapp/index.html', {
         'form': form,
     })
+
+def geocode(name):
+	ENCODING = 'utf-8'
+	url = u"http://maps.google.com/maps/api/geocode/xml?&language=ja&sensor=false&region=ja&address="
+
+	url = url + urllib.quote(name.encode(ENCODING))
+
+	buffer = urllib.urlopen(url).read()
+
+	return buffer
+
+
